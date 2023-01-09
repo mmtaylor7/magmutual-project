0db9bee832ba75757655ca041a65b296d057747a
mmtaylor <taylor.marymargaret@gmail.com>
2023-01-09 12:27:56 -0500

    wrapped up before beginning backend

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,6 @@ import Profession from "./components/profession/Profession";
 import Location from "./components/location/Location";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState();
-
   return (
     <div className="App">
       <Header />
