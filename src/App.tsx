4ab52ec88a30a84053fc98f1a39d73d252a2c4bc
mmtaylor <taylor.marymargaret@gmail.com>
2023-01-17 14:14:03 -0500

    delete unused import

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import React, { useState } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "bootstrap";
 import "react-bootstrap";
-
 import NavBar from "./components/navBar/NavBar";
 import User from "./components/user/User";
 import Date from "./components/date/Date";
 import Profession from "./components/profession/Profession";
 import Location from "./components/location/Location";
 import UserProfile from "./components/userProfile/UserProfile";
-import { PictureAsPdfOutlined } from "@mui/icons-material";
 
 function App() {
   const [users, setUsers] = useState([]);
